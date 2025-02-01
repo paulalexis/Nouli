eab73293b066b1db5076800ae1b8fe42519345f8
Paul-PTS <Paulalexismor@gmail.com>
2025-02-01 22:25:33 +0100

    test4

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     thread = threading.Thread(target=background_task, daemon=True)
     thread.start()
 
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
